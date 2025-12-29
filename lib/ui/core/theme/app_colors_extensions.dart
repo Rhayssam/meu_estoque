0a24469f3f1f9c08d68c0b2b81d94247f959f598
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 10:42:39 -0300

    feat: adicionando o AppColorsExtensions

---
+++ lib/ui/core/theme/app_colors_extensions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColorsExtensions extends ThemeExtension<AppColorsExtensions> {
+  AppColorsExtensions();
+
+  @override
+  ThemeExtension<AppColorsExtensions> copyWith({Color? success}) {
+    return AppColorsExtensions();
+  }
+
+  @override
+  ThemeExtension<AppColorsExtensions> lerp(covariant ThemeExtension<AppColorsExtensions>? other, double t) {
+    return AppColorsExtensions();
+  }
+}
