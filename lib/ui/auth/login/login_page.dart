d6b6fced53deba8a247f0a2f6caf630d2e872bd0
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 14:27:07 -0300

    feat: adicionando o CustomButton primary na LoginPage

--- lib/ui/auth/login/login_page.dart
+++ lib/ui/auth/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meu_estoque/ui/core/widgets/buttons/custom_button.dart';
 import 'package:meu_estoque/ui/core/widgets/double_click_to_exit/double_click_to_exit.dart';
 
 class LoginPage extends StatelessWidget {
@@ -8,29 +9,36 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return DoubleClickToExit(
       child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  spacing: 20,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        label: Text('Login'),
-                      ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                spacing: 20,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text('Login'),
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        label: Text('Senha'),
-                      ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text('Senha'),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: CustomButton.primary(
+                      onPressed: () {},
+                      icon: Icon(Icons.login),
+                      label: Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
