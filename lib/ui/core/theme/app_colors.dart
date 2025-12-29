e4a8f40a20218f6fa41e94c93ffed9793e7517cd
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 10:42:44 -0300

    feat: adicionando o AppColors

---
+++ lib/ui/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final class AppColors {
+  static final ColorScheme lightColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFFF16C04),
+    onPrimary: Color(0xFFFFFFFF),
+    secondary: Color(0xFFFF5900),
+    onSecondary: Color(0xFFFFFFFF),
+    error: Color(0xFFFF4E4E),
+    onError: Color(0xFFF9F9F9),
+    primaryContainer: Color(0xFFFFFFFF),
+    onPrimaryContainer: Color(0xFF000000),
+    surface: Color(0xFFF3F3F3),
+    onSurface: Color(0XFF444444),
+  );
+}
