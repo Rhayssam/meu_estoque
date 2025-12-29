9907f1de616de64e968a1374635ca8b72dd6615c
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 17:59:32 -0300

    feat: adicionando AppConstants

--- lib/config/constants/app_constants.dart
+++ lib/config/constants/app_constants.dart
@@ -2,4 +2,10 @@ final class AppConstants {
   static const String appTitle = 'Meu Estoque';
 
   static const bool kDevicePreviewEnable = false;
+
+  static const bool kAutofillCompanyDocumentation = false;
+
+  static const bool kFillLoginForms = true;
+  static const String kLoginEmail = 'rhayssam@email.com.br';
+  static const String kLoginPassword = 'Teste@1234';
 }
