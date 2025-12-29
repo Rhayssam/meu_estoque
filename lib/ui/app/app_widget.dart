3542b0d891f43af038ee035caf27ef4787bc493a
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 10:42:04 -0300

    feat: adicionando o AppWidget

---
+++ lib/ui/app/app_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meu_estoque/config/constants/app_constants.dart';
+import 'package:meu_estoque/routing/app_router.dart';
+import 'package:meu_estoque/routing/routes.dart';
+import 'package:meu_estoque/ui/app/app_binding.dart';
+import 'package:meu_estoque/ui/core/theme/app_theme.dart';
+import 'package:meu_estoque/ui/core/theme/theme_controller.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  ThemeController get _themeController => Get.find<ThemeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _themeController.syncTheme(MediaQuery.platformBrightnessOf(context)),
+    );
+    return GetMaterialApp(
+      title: AppConstants.appTitle,
+      initialBinding: AppBindings(),
+      locale: Locale('pt'),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light(),
+      initialRoute: Routes.dev,
+      getPages: AppRouter.pages,
+      unknownRoute: AppRouter.unknownRoutePage,
+    );
+  }
+}
