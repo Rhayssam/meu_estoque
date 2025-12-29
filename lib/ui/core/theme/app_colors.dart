3891fea0f3dc03246a7a91e9cfbeefe6d400a344
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 10:54:16 -0300

    refact: trocando as cores no AppColors e criando a flutter_native_splash

--- lib/ui/core/theme/app_colors.dart
+++ lib/ui/core/theme/app_colors.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 final class AppColors {
   static final ColorScheme lightColorScheme = ColorScheme(
     brightness: Brightness.light,
-    primary: Color(0xFFF16C04),
+    primary: Color(0xFF0473F1),
     onPrimary: Color(0xFFFFFFFF),
-    secondary: Color(0xFFFF5900),
+    secondary: Color(0xFF59A2F5),
     onSecondary: Color(0xFFFFFFFF),
     error: Color(0xFFFF4E4E),
     onError: Color(0xFFF9F9F9),
