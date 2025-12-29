8506edfaba9705d5883d6b53823dd52d73c63c3f
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 10:43:39 -0300

    feat: adicionando o CustomAppBar

---
+++ lib/ui/core/widgets/app_bar/custom_app_bar.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:meu_estoque/ui/core/widgets/app_bar/custom_app_bar_styles.dart';
+
+enum CustomAppBarThemeEnum {
+  custom,
+  primary,
+  transparent,
+  surface,
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    this.actions,
+    this.leading,
+    this.title,
+    this.theme,
+  }) : customAppBarThemeEnum = CustomAppBarThemeEnum.custom;
+
+  const CustomAppBar.transparent({
+    super.key,
+    this.actions,
+    this.leading,
+    this.title,
+  }) : customAppBarThemeEnum = CustomAppBarThemeEnum.transparent,
+       theme = null;
+
+  const CustomAppBar.primary({
+    super.key,
+    this.actions,
+    this.leading,
+    this.title,
+  }) : customAppBarThemeEnum = CustomAppBarThemeEnum.primary,
+       theme = null;
+
+  const CustomAppBar.surface({
+    super.key,
+    this.actions,
+    this.leading,
+    this.title,
+  }) : customAppBarThemeEnum = CustomAppBarThemeEnum.surface,
+       theme = null;
+
+  final CustomAppBarThemeEnum customAppBarThemeEnum;
+  final List<Widget>? actions;
+  final Widget? leading;
+  final Widget? title;
+  final AppBarTheme? theme;
+
+  AppBarTheme? _getAppBarTheme(BuildContext context) {
+    return switch (customAppBarThemeEnum) {
+      CustomAppBarThemeEnum.custom => theme,
+      CustomAppBarThemeEnum.primary => Theme.of(context).primaryAppBarTheme,
+      CustomAppBarThemeEnum.transparent => Theme.of(context).transparentAppBarTheme,
+      CustomAppBarThemeEnum.surface => Theme.of(context).surfaceAppBarTheme,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AppBarTheme? theme = _getAppBarTheme(context);
+
+    return AppBar(
+      elevation: theme?.elevation,
+      iconTheme: theme?.iconTheme,
+      backgroundColor: theme?.backgroundColor,
+      actionsIconTheme: theme?.actionsIconTheme,
+      titleTextStyle: theme?.titleTextStyle,
+      centerTitle: theme?.centerTitle,
+      shadowColor: theme?.shadowColor,
+      foregroundColor: theme?.foregroundColor,
+      surfaceTintColor: theme?.surfaceTintColor,
+      shape: theme?.shape,
+      leading: leading,
+      title: title,
+      actions: actions,
+      automaticallyImplyLeading: false,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
