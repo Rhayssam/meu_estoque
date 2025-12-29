d0c9c002f6c5cd2e0fa92d538dd3944c3f2db161
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 17:59:56 -0300

    feat: adicionando saveUser no UserSessionStorage

--- lib/data/services/local_storage/session/user_session_storage.dart
+++ lib/data/services/local_storage/session/user_session_storage.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meu_estoque/data/services/local_storage/shared_preferences/shared_preferences_local_storage.dart';
+import 'package:meu_estoque/domain/user_model.dart';
 
 class UserSessionStorage {
   final SharedPreferencesLocalStorage _storage;
   UserSessionStorage(this._storage);
 
+  final String _token = "token";
+  final String _name = "name";
+  final String _id = "id";
+  final String _email = "email";
   final String _themeMode = "theme_mode";
 
   Future<void> setThemeMode(ThemeMode mode) async {
     String value = mode.name;
     await _storage.setString(_themeMode, value);
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    await _storage.setString(_token, user.token);
+    await _storage.setString(_name, user.name);
+    await _storage.setString(_id, user.uuid);
   }
 
   ThemeMode? getThemeMode() {
@@ -25,5 +36,22 @@ class UserSessionStorage {
       default:
         return ThemeMode.system;
     }
+  }
+
+  UserModel? getUser() {
+    final token = _storage.getString(_token);
+    final name = _storage.getString(_name);
+    final id = _storage.getString(_id);
+    final email = _storage.getString(_email);
+
+    if (token != null && name != null && id != null && email != null) {
+      return UserModel(
+        uuid: id,
+        name: name,
+        token: token,
+        email: email,
+      );
+    }
+    return null;
   }
 }
