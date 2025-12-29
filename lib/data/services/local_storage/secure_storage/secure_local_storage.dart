98152bea96077ecf5abab65d382a98f46e3cf3b6
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 16:28:02 -0300

    feat: adicionando o SecureLocalStorage

---
+++ lib/data/services/local_storage/secure_storage/secure_local_storage.dart
@@ -0,0 +1,7 @@
+abstract interface class SecureLocalStorage {
+  Future<void> write(String key, String value);
+  Future<String?> read(String key);
+  Future<void> delete(String key);
+  Future<void> deleteAll();
+  Future<bool> containsKey(String key);
+}
