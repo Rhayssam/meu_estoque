bf2ee8d7eed251e2bfef84f2b3d61a5461cde768
Rhayssam <rhayssam.hv@gmail.com>
2025-12-29 10:42:28 -0300

    feat: adicionando o LoginPage

---
+++ lib/ui/auth/login/login_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meu_estoque/ui/core/widgets/double_click_to_exit/double_click_to_exit.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DoubleClickToExit(
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  spacing: 20,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text('data'),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text('data'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
