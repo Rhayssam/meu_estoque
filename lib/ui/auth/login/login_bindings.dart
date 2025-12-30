b834cb4d4f1ddbfd693733cc20fbe6be5492a6c6
Rhayssam <rhayssam.hv@gmail.com>
2025-12-30 08:27:00 -0300

    feat: adicionando as dependencias no LoginBindings

--- lib/ui/auth/login/login_bindings.dart
+++ lib/ui/auth/login/login_bindings.dart
@@ -1,9 +1,28 @@
 import 'package:get/get.dart';
+import 'package:meu_estoque/data/repositories/auth/auth_repository.dart';
+import 'package:meu_estoque/data/repositories/auth/auth_repository_mock.dart';
+import 'package:meu_estoque/data/services/api/dio/client_http.dart';
+import 'package:meu_estoque/data/services/local_storage/session/user_session_storage.dart';
+import 'package:meu_estoque/data/services/local_storage/token_storage/token_storage.dart';
+import 'package:meu_estoque/data/services/logger/logger.dart';
 import 'package:meu_estoque/ui/auth/login/login_controller.dart';
 
 class LoginBindings implements Bindings {
   @override
   void dependencies() {
-    Get.put(LoginController());
+    Get.lazyPut(
+      () => AuthRepositoryMock(
+        client: Get.find<ClientHttp>(),
+        log: Get.find<Logger>(),
+        tokenStorage: Get.find<TokenStorage>(),
+      ),
+    );
+    Get.put(
+      LoginController(
+        authRepository: Get.find<AuthRepository>(),
+        userSessionStorage: Get.find<UserSessionStorage>(),
+        log: Get.find<Logger>(),
+      ),
+    );
   }
 }
