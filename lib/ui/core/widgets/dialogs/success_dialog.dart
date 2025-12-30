a3b9f6741399e7ce9360d3fbe20040d75dd57717
Rhayssam <rhayssam.hv@gmail.com>
2025-12-30 08:33:33 -0300

    feat: adicionando o SuccessDialog

---
+++ lib/ui/core/widgets/dialogs/success_dialog.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:meu_estoque/ui/core/widgets/buttons/custom_button.dart';
+import 'package:meu_estoque/ui/core/widgets/dialogs/custom_dialog.dart';
+
+class SuccessDialog extends StatelessWidget {
+  const SuccessDialog({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.buttonLabel,
+    required this.onPressed,
+  });
+
+  final String title;
+  final String content;
+  final String buttonLabel;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return CustomDialog(
+      icon: Icon(
+        CupertinoIcons.check_mark_circled,
+        size: 60,
+      ),
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: theme.textTheme.titleLarge?.copyWith(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: Text(
+        content,
+        textAlign: TextAlign.center,
+        style: theme.textTheme.bodyMedium,
+      ),
+      actions: [
+        Expanded(
+          child: CustomButton.primary(
+            onPressed: onPressed,
+            label: Text(buttonLabel),
+          ),
+        ),
+      ],
+    );
+  }
+}
