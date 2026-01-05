bcdd83e2ac866fbe76f0b696c895191c68319b78
Rhayssam <rhayssam.hv@gmail.com>
2026-01-05 16:09:46 -0300

    feat: adicionando a Drawer na HomePage

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 import 'package:meu_estoque/ui/core/widgets/app_bar/app_bar_back_button.dart';
 import 'package:meu_estoque/ui/core/widgets/app_bar/custom_app_bar.dart';
+import 'package:meu_estoque/ui/home/widgets/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar.primary(
-        leading: AppBarBackButton(
-          onPressed: () => Get.back(),
-        ),
-        title: const Text('HomePage'),
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
       ),
-      body: Column(),
+      child: SafeArea(
+        child: Scaffold(
+          drawer: HomeDrawer(),
+          appBar: CustomAppBar.primary(
+            title: const Text('HomePage'),
+            leading: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                );
+              },
+            ),
+          ),
+          body: Column(
+            children: [Text('data')],
+          ),
+        ),
+      ),
     );
   }
 }
