3ec5e27e1019415565d25bb35c37864a23dcd208
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 16:26:00 -0300

    refact: change CompanyServicesExtensions to ServicesExtensions

--- lib/domain/extensions/services_extensions.dart
+++ lib/domain/extensions/services_extensions.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meu_estoque/domain/enums/services_enum.dart';
 
-extension CompanyServicesExtensions on ServicesEnum {
+extension ServicesExtensions on ServicesEnum {
   IconData get icon => switch (this) {
     ServicesEnum.companies => Icons.list,
   };
