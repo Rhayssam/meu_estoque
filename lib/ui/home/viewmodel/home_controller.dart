262611168d3463d2d5e88751bc6e7c9b1b8a20d5
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 15:14:27 -0300

    feat: adicionando o state na HomeController

--- lib/ui/home/viewmodel/home_controller.dart
+++ lib/ui/home/viewmodel/home_controller.dart
@@ -1,3 +1,17 @@
-import 'package:get/get.dart';
+import 'dart:async';
 
-class HomeController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:meu_estoque/ui/home/viewmodel/home_states.dart';
+
+class HomeController extends GetxController {
+  final StreamController<HomeStates> _homeStateController = StreamController<HomeStates>.broadcast();
+  final Rx<HomeStates> _homeState = Rx<HomeStates>(HomeInitialState());
+
+  HomeStates get state => _homeState.value;
+
+  @override
+  void onInit() async {
+    _homeState.bindStream(_homeStateController.stream);
+    super.onInit();
+  }
+}
