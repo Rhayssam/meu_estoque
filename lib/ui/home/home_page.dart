4eda1312f6b758c729818310b975fc5626691c43
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 15:14:58 -0300

    feat: adicionando o CompanyServiceCard na HomePage

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:meu_estoque/domain/enums/services_enum.dart';
 import 'package:meu_estoque/ui/core/widgets/app_bar/custom_sliver_app_bar.dart';
+import 'package:meu_estoque/ui/home/viewmodel/home_controller.dart';
+import 'package:meu_estoque/ui/home/viewmodel/home_states.dart';
+import 'package:meu_estoque/ui/home/widgets/company_service_card.dart';
 import 'package:meu_estoque/ui/home/widgets/drawer/home_drawer.dart';
 import 'package:meu_estoque/ui/home/widgets/home_header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  HomeController get _controller => Get.find<HomeController>();
 
   @override
   Widget build(BuildContext context) {
@@ -47,10 +54,25 @@ class HomePage extends StatelessWidget {
                     children: [
                       Column(
                         children: [
-                          Text(
-                            'Opções',
-                            style: Theme.of(context).textTheme.headlineSmall,
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Opções',
+                              style: Theme.of(context).textTheme.headlineSmall,
+                            ),
                           ),
+                          for (final service in ServicesEnum.values)
+                            CompanyServiceCard(
+                              service: service,
+                              onTap: (service) {
+                                if (_controller.state is HomeLoadingCompaniesState) {
+                                  return;
+                                }
+                                Get.toNamed(
+                                  service.route,
+                                );
+                              },
+                            ),
                         ],
                       ),
                     ],
