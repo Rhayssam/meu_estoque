6ce660d56b6ecfb9e357c82119cd68e8498cdbfd
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 16:48:19 -0300

    rename: trocando o companies por products no ServicesEnum

--- lib/domain/enums/services_enum.dart
+++ lib/domain/enums/services_enum.dart
@@ -1,7 +1,7 @@
 import 'package:meu_estoque/routing/routes.dart';
 
 enum ServicesEnum {
-  companies('Meus Produtos', Routes.productList),
+  products('Meus Produtos', Routes.productList),
   ;
 
   const ServicesEnum(this.label, this.route);
