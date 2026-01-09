251c2c175dca93df90c2f769dad0411420576970
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 17:21:17 -0300

    feat: adicionando o sales no ServicesEnum

--- lib/domain/enums/services_enum.dart
+++ lib/domain/enums/services_enum.dart
@@ -2,6 +2,7 @@ import 'package:meu_estoque/routing/routes.dart';
 
 enum ServicesEnum {
   products('Meus Produtos', Routes.productList),
+  sales('Vendas', Routes.sales),
   ;
 
   const ServicesEnum(this.label, this.route);
