2ec77f1fc894a3a3d9c77f9ffe917bab5c7e2667
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 16:48:37 -0300

    rename: trocando o companies por products no ServicesExtensions e trocando o icone

--- lib/domain/extensions/services_extensions.dart
+++ lib/domain/extensions/services_extensions.dart
@@ -3,6 +3,6 @@ import 'package:meu_estoque/domain/enums/services_enum.dart';
 
 extension ServicesExtensions on ServicesEnum {
   IconData get icon => switch (this) {
-    ServicesEnum.companies => Icons.list,
+    ServicesEnum.products => Icons.toys,
   };
 }
