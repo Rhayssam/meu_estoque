b0d91797f09ef0bde9095c20b8e7f00204407dba
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 13:55:45 -0300

    feat: adicionando o HomeHeader na HomePage e adicionando os slivers

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:meu_estoque/ui/core/widgets/app_bar/custom_app_bar.dart';
+import 'package:meu_estoque/ui/core/widgets/app_bar/custom_sliver_app_bar.dart';
 import 'package:meu_estoque/ui/home/widgets/drawer/home_drawer.dart';
-import 'package:meu_estoque/ui/home/widgets/greetings.dart';
+import 'package:meu_estoque/ui/home/widgets/home_header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,49 +16,28 @@ class HomePage extends StatelessWidget {
       child: SafeArea(
         child: Scaffold(
           drawer: HomeDrawer(),
-          appBar: CustomAppBar.tertiaryFixed(
-            title: const Text('MeuEstoque App'),
-            leading: Builder(
-              builder: (context) {
-                return IconButton(
-                  icon: const Icon(Icons.menu),
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
+          body: CustomScrollView(
+            scrollDirection: Axis.vertical,
+            physics: const AlwaysScrollableScrollPhysics(),
+            slivers: [
+              CustomSliverAppBar.tertiaryFixed(
+                title: const Text('MeuEstoque App'),
+                leading: Builder(
+                  builder: (context) {
+                    return IconButton(
+                      icon: const Icon(Icons.menu),
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                    );
                   },
-                );
-              },
-            ),
-          ),
-          body: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.tertiaryFixed,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Theme.of(context).colorScheme.shadow.withValues(alpha: 0.16),
-                      spreadRadius: 5,
-                      blurRadius: 10,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
                 ),
-
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 20,
-                    right: 20,
-                    top: 20,
-                    bottom: 30,
-                  ),
-                  child: Column(
-                    spacing: 10,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Greetings(),
-                    ],
-                  ),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    HomeHeader(),
+                  ],
                 ),
               ),
             ],
