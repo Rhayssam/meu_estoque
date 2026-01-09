a2e1d7f8ae729acbf5ef293b43a0bb187ca4a218
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 11:49:09 -0300

    feat: add AboutCard

---
+++ lib/ui/about_us/widgets/about_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AboutCard extends StatelessWidget {
+  const AboutCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.1),
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        border: Border.all(color: Theme.of(context).colorScheme.onPrimaryContainer),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(
+              thickness: 0.3,
+              color: Theme.of(context).colorScheme.primaryFixed,
+            ),
+            Text(subtitle),
+          ],
+        ),
+      ),
+    );
+  }
+}
