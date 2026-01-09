f10a99fea03156d35d8423e72d98f2a93ac45307
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 17:21:07 -0300

    feat: adicionando o sales no ServicesExtensions

--- lib/domain/extensions/services_extensions.dart
+++ lib/domain/extensions/services_extensions.dart
@@ -4,5 +4,6 @@ import 'package:meu_estoque/domain/enums/services_enum.dart';
 extension ServicesExtensions on ServicesEnum {
   IconData get icon => switch (this) {
     ServicesEnum.products => Icons.toys,
+    ServicesEnum.sales => Icons.sell,
   };
 }
