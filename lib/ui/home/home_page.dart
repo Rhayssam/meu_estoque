91a3866436ba891965dc0552103376e3d9ff3cb9
Rhayssam <rhayssam.hv@gmail.com>
2026-01-09 10:11:25 -0300

    feat: adicionando o Greetings na HomePage

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:meu_estoque/ui/core/widgets/app_bar/custom_app_bar.dart';
 import 'package:meu_estoque/ui/home/widgets/drawer/home_drawer.dart';
+import 'package:meu_estoque/ui/home/widgets/greetings.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,7 +17,7 @@ class HomePage extends StatelessWidget {
         child: Scaffold(
           drawer: HomeDrawer(),
           appBar: CustomAppBar.tertiaryFixed(
-            title: const Text('HomePage'),
+            title: const Text('MeuEstoque App'),
             leading: Builder(
               builder: (context) {
                 return IconButton(
@@ -29,7 +30,38 @@ class HomePage extends StatelessWidget {
             ),
           ),
           body: Column(
-            children: [Text('data')],
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.tertiaryFixed,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Theme.of(context).colorScheme.shadow.withValues(alpha: 0.16),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: 20,
+                    bottom: 30,
+                  ),
+                  child: Column(
+                    spacing: 10,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Greetings(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
