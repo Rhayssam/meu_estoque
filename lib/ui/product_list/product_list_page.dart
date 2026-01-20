482a152916feab40aec171188191ef64c91d7404
Rhayssam <rhayssam.hv@gmail.com>
2026-01-20 17:25:52 -0300

    feat: Adicionando um CustomScrollView com a CustomSliverAppBar na ProductListPage

--- lib/ui/product_list/product_list_page.dart
+++ lib/ui/product_list/product_list_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meu_estoque/ui/core/widgets/app_bar/app_bar_back_button.dart';
+import 'package:meu_estoque/ui/core/widgets/app_bar/custom_sliver_app_bar.dart';
 
 class ProductListPage extends StatelessWidget {
   const ProductListPage({super.key});
@@ -6,10 +9,21 @@ class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProductsListPage'),
+      body: CustomScrollView(
+        slivers: [
+          CustomSliverAppBar.surface(
+            title: Text('Produtos'),
+            leading: AppBarBackButton(onPressed: Get.back),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Text('data'),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
