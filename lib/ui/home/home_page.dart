273f3a117ebe765dd3fa16b4227706b43541a3cb
Rhayssam <rhayssam.hv@gmail.com>
2026-01-20 16:26:09 -0300

    feat: adicionando o SafeArea na HomePage

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:meu_estoque/ui/core/widgets/app_bar/custom_sliver_app_bar.dart';
 import 'package:meu_estoque/ui/home/viewmodel/home_controller.dart';
 import 'package:meu_estoque/ui/home/widgets/drawer/home_drawer.dart';
-import 'package:meu_estoque/ui/home/widgets/home_header.dart';
 import 'package:meu_estoque/ui/home/widgets/services.dart';
 
 class HomePage extends StatelessWidget {
@@ -20,94 +19,96 @@ class HomePage extends StatelessWidget {
       ),
       child: Scaffold(
         drawer: HomeDrawer(),
-        body: CustomScrollView(
-          scrollDirection: Axis.vertical,
-          physics: const AlwaysScrollableScrollPhysics(),
-          slivers: [
-            CustomSliverAppBar.surface(
-              title: const Text('MeuEstoque App'),
-              leading: Builder(
-                builder: (context) {
-                  return IconButton(
-                    icon: const Icon(Icons.menu),
-                    onPressed: () {
-                      Scaffold.of(context).openDrawer();
-                    },
-                  );
-                },
+        body: SafeArea(
+          child: CustomScrollView(
+            scrollDirection: Axis.vertical,
+            physics: const AlwaysScrollableScrollPhysics(),
+            slivers: [
+              CustomSliverAppBar.surface(
+                title: const Text('MeuEstoque App'),
+                leading: Builder(
+                  builder: (context) {
+                    return IconButton(
+                      icon: const Icon(Icons.menu),
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-            // SliverToBoxAdapter(
-            //   child: HomeHeader(),
-            // ),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.all(20),
-              sliver: SliverToBoxAdapter(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
+              // SliverToBoxAdapter(
+              //   child: HomeHeader(),
+              // ),
+              SliverPadding(
+                padding: EdgeInsetsGeometry.all(20),
+                sliver: SliverToBoxAdapter(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
 
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withValues(alpha: 0.1),
-                        blurRadius: 5,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      spacing: 5,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Text(
-                            'DASHBOARD',
-                            style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
-                          ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.1),
+                          blurRadius: 5,
+                          offset: Offset(0, 2),
                         ),
-                        Divider(
-                          color: Theme.of(context).colorScheme.primaryFixed.withAlpha(70),
-                        ),
-                        Text('Vendas da semana: '),
-                        Text('Vendas do mês: '),
                       ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        spacing: 5,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: Text(
+                              'DASHBOARD',
+                              style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Divider(
+                            color: Theme.of(context).colorScheme.primaryFixed.withAlpha(70),
+                          ),
+                          Text('Vendas da semana: '),
+                          Text('Vendas do mês: '),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
-              sliver: SliverToBoxAdapter(
-                child: Divider(
-                  color: Theme.of(context).colorScheme.primaryFixed.withAlpha(70),
+              SliverPadding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+                sliver: SliverToBoxAdapter(
+                  child: Divider(
+                    color: Theme.of(context).colorScheme.primaryFixed.withAlpha(70),
+                  ),
                 ),
               ),
-            ),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
-              sliver: SliverToBoxAdapter(
-                child: Text('Data'),
-              ),
-            ),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
-              sliver: SliverToBoxAdapter(
-                child: Divider(
-                  color: Theme.of(context).colorScheme.primaryFixed.withAlpha(70),
+              SliverPadding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+                sliver: SliverToBoxAdapter(
+                  child: Text('Data'),
                 ),
               ),
-            ),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.all(20),
-              sliver: SliverToBoxAdapter(
-                child: Services(controller: _controller),
+              SliverPadding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+                sliver: SliverToBoxAdapter(
+                  child: Divider(
+                    color: Theme.of(context).colorScheme.primaryFixed.withAlpha(70),
+                  ),
+                ),
               ),
-            ),
-          ],
+              SliverPadding(
+                padding: EdgeInsetsGeometry.all(20),
+                sliver: SliverToBoxAdapter(
+                  child: Services(controller: _controller),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
