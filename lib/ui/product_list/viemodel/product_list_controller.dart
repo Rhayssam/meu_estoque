3d9e4e230aa5e918fa4f7e8d4f3b2e8c219069d9
Rhayssam <rhayssam.hv@gmail.com>
2026-01-22 17:56:47 -0300

    feat: adicionando o goToProductAddPage na ProductListController

--- lib/ui/product_list/viemodel/product_list_controller.dart
+++ lib/ui/product_list/viemodel/product_list_controller.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:meu_estoque/routing/routes.dart';
 
 class ProductListController extends GetxController {
   final fabScrollController = ScrollController();
+  Future<void> goToProductAddPage() async {
+    await Get.toNamed(Routes.productAdd);
+  }
 }
