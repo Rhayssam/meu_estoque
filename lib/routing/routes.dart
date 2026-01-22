10904e68bedd8e43f269ce8736a1934ca4335c86
Rhayssam <rhayssam.hv@gmail.com>
2026-01-22 17:55:49 -0300

    feat: adicionando o productAdd no Routes

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -4,6 +4,7 @@ final class Routes {
   static const String home = '/home';
   static const String about = '/about';
   static const String productList = '/product/list';
+  static const String productAdd = '/product/add';
   static const String sales = '/sales';
   static const String inventory = '/inventory';
 
