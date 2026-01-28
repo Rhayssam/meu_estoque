3c13c5d1d0dbfeff27270254e97d43f06a3822b2
Rhayssam <rhayssam.hv@gmail.com>
2026-01-28 16:48:07 -0300

    feat: add RegisterPage

---
+++ lib/ui/auth/register/register_page.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'viewmodel/register_controller.dart';
+
+class RegisterPage extends GetView<RegisterController> {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RegisterPage'),
+      ),
+      body: SafeArea(
+        child: CustomScrollView(slivers: []),
+      ),
+    );
+  }
+}
