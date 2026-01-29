0118386be11f086db4ae95a6e6cd0f9f2b494031
Rhayssam <rhayssam.hv@gmail.com>
2026-01-29 16:29:57 -0300

    feat: add routes login as initialRoute on AppWidget

--- lib/ui/app/app_widget.dart
+++ lib/ui/app/app_widget.dart
@@ -23,8 +23,8 @@ class AppWidget extends StatelessWidget {
       locale: Locale('pt'),
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
-      initialRoute: Routes.dev,
-      // initialRoute: Routes.login,
+      // initialRoute: Routes.dev,
+      initialRoute: Routes.login,
       getPages: AppRouter.pages,
       unknownRoute: AppRouter.unknownRoutePage,
     );
