1fbbad63d8f9a2236b3abbd2731885ca3dd54269
Rhayssam <rhayssam.hv@gmail.com>
2026-01-30 14:22:19 -0300

    refact: change onInit to onReady on SplashController

--- lib/ui/splash/viewmodel/splash_controller.dart
+++ lib/ui/splash/viewmodel/splash_controller.dart
@@ -3,12 +3,11 @@ import 'package:meu_estoque/routing/routes.dart';
 
 class SplashController extends GetxController {
   @override
-  void onInit() {
-    Future.delayed(const Duration(seconds: 2)).then(
-      (value) async {
-        Get.offAllNamed(Routes.login);
-      },
-    );
-    super.onInit();
+  void onReady() {
+    super.onReady();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.offAllNamed(Routes.login);
+    });
   }
 }
