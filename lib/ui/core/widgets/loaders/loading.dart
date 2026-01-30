984d6724a731577ca00b52674dbb4190626321ec
Rhayssam <rhayssam.hv@gmail.com>
2026-01-30 09:13:36 -0300

    feat: add LoadingAnimationWidget on Loading

--- lib/ui/core/widgets/loaders/loading.dart
+++ lib/ui/core/widgets/loaders/loading.dart
@@ -1,1 +1,16 @@
-class Loading {}
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LoadingAnimationWidget.progressiveDots(
+        color: Theme.of(context).colorScheme.primary,
+        size: 50,
+      ),
+    );
+  }
+}
