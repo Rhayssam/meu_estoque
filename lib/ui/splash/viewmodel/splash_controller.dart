f09b4d3f7b9df9ecef6a393b453c604d48f2c985
Rhayssam <rhayssam.hv@gmail.com>
2026-01-30 14:50:51 -0300

    feat: add startFlow and onClose on SplashController

--- lib/ui/splash/viewmodel/splash_controller.dart
+++ lib/ui/splash/viewmodel/splash_controller.dart
@@ -1,13 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:meu_estoque/routing/routes.dart';
 
-class SplashController extends GetxController {
-  @override
-  void onReady() {
-    super.onReady();
+class SplashController extends GetxController with GetSingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation<double> revealAnimation;
 
-    Future.delayed(const Duration(seconds: 2), () {
-      Get.offAllNamed(Routes.login);
-    });
+  @override
+  void onInit() {
+    super.onInit();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 900),
+    );
+
+    revealAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeInOut,
+    );
+
+    _startFlow();
+  }
+
+  Future<void> _startFlow() async {
+    await animationController.forward();
+
+    await Future.delayed(const Duration(seconds: 1));
+
+    await animationController.reverse();
+
+    Get.offAllNamed(
+      Routes.login,
+    );
+  }
+
+  @override
+  void onClose() {
+    animationController.dispose();
+    super.onClose();
   }
 }
