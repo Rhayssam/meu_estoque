9473d5d68c35f55b126890fae5759e4342c9f16d
Rhayssam <rhayssam.hv@gmail.com>
2026-02-03 11:49:09 -0300

    feat: add formatted day on HomeController

--- lib/ui/home/viewmodel/home_controller.dart
+++ lib/ui/home/viewmodel/home_controller.dart
@@ -1,17 +1,21 @@
 import 'dart:async';
 
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:meu_estoque/routing/routes.dart';
 import 'package:meu_estoque/ui/home/viewmodel/home_states.dart';
 
 class HomeController extends GetxController {
   final StreamController<HomeStates> _homeStateController = StreamController<HomeStates>.broadcast();
+
   final Rx<HomeStates> _homeState = Rx<HomeStates>(HomeInitialState());
 
   HomeStates get state => _homeState.value;
 
+  final String todayFormatted = DateFormat('dd MMM yyyy', 'pt_BR').format(DateTime.now());
+
   @override
-  void onInit() async {
+  void onInit() {
     _homeState.bindStream(_homeStateController.stream);
     super.onInit();
   }
