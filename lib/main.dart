a321f1dcd57119b3a6946ec9b96aa6a6d5dc54bf
Rhayssam <rhayssam.hv@gmail.com>
2026-02-03 11:46:18 -0300

    feat: add intl on Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:meu_estoque/config/constants/app_constants.dart';
 import 'package:meu_estoque/dependencies.dart';
 import 'package:meu_estoque/ui/app/app_widget.dart';
@@ -8,15 +9,17 @@ import 'package:meu_estoque/ui/app/app_widget.dart';
 Future<void> main(List<String> args) async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await initializeDateFormatting('pt_BR', null);
+
   await Dependencies().initialize();
+
   FlutterNativeSplash.remove();
 
   runApp(
     DevicePreview(
       enabled: AppConstants.kDevicePreviewEnable,
-      builder: (_) {
-        return AppWidget();
-      },
+      builder: (_) => const AppWidget(),
     ),
   );
 }
