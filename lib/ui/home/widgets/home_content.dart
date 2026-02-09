85ec792500b035231d107fd2d77c75e89b8d8f03
Rhayssam <rhayssam.hv@gmail.com>
2026-02-09 14:17:35 -0300

    feat: remove Expanded from HomeContent

--- lib/ui/home/widgets/home_content.dart
+++ lib/ui/home/widgets/home_content.dart
@@ -10,66 +10,64 @@ class HomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ClipRRect(
-        borderRadius: BorderRadiusGeometry.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(25),
-          color: theme.colorScheme.surface,
-          child: Center(
-            child: Column(
-              spacing: 15,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Histórico',
-                      style: theme.textTheme.titleMedium?.copyWith(
-                        color: theme.colorScheme.onSecondary,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                    Icon(
-                      Icons.more_horiz,
+    return ClipRRect(
+      borderRadius: BorderRadiusGeometry.only(
+        topLeft: Radius.circular(25),
+        topRight: Radius.circular(25),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        color: theme.colorScheme.surface,
+        child: Center(
+          child: Column(
+            spacing: 15,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Histórico',
+                    style: theme.textTheme.titleMedium?.copyWith(
                       color: theme.colorScheme.onSecondary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: theme.colorScheme.onSecondary,
+                  ),
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0, 2),
+                      blurRadius: 4,
                     ),
                   ],
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.secondary,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        offset: Offset(0, 2),
-                        blurRadius: 4,
-                      ),
-                    ],
+                child: ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text(
+                    'Produto Cadastrado',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
                   ),
-                  child: ListTile(
-                    leading: Icon(Icons.favorite),
-                    title: Text(
-                      'Produto Cadastrado',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      'Um novo produto foi cadastradodasdasdsadadasdasdas',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: theme.textTheme.labelMedium,
-                    ),
+                  subtitle: Text(
+                    'Um novo produto foi cadastradodasdasdsadadasdasdas',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: theme.textTheme.labelMedium,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
