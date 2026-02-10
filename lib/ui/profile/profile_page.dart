33ea32ef805d8bf4abf4ab34b4c002dca004d6a4
Rhayssam <rhayssam.hv@gmail.com>
2026-02-10 17:31:17 -0300

    feat: add ProfileHeader and ProfileOptionCard on ProfilePage

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:meu_estoque/ui/profile/widgets/profile_header.dart';
+import 'package:meu_estoque/ui/profile/widgets/profile_option_card.dart';
 import 'viewmodel/profile_controller.dart';
 
 class ProfilePage extends GetView<ProfileController> {
@@ -12,64 +14,9 @@ class ProfilePage extends GetView<ProfileController> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(color: theme.colorScheme.secondary),
-              child: Row(
-                spacing: 15,
-                children: [
-                  CircleAvatar(
-                    radius: 28,
-                  ),
-                  Column(
-                    spacing: 4,
-                    children: [
-                      Text(
-                        'Bem-vindo, usuário',
-                        style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        spacing: 6,
-                        children: [
-                          Icon(
-                            Icons.email,
-                            color: theme.colorScheme.primary,
-                            size: 15,
-                          ),
-                          Text(
-                            'emaillegal@email.com',
-                            style: theme.textTheme.bodySmall,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            ProfileHeader(theme: theme),
             const SizedBox(height: 30),
-            Material(
-              color: theme.colorScheme.secondary,
-              child: InkWell(
-                onTap: () {},
-                child: Container(
-                  padding: EdgeInsets.all(18),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Meu Perfil',
-                        style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        size: 18,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            ProfileOptionCard(theme: theme),
           ],
         ),
       ),
