18270f26ab504ca250dab952e569fb2da84236ab
Rhayssam <rhayssam.hv@gmail.com>
2026-02-10 11:02:32 -0300

    feat: add container with email and name on ProfilePage

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -7,11 +7,49 @@ class ProfilePage extends GetView<ProfileController> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProfilePage'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(color: theme.colorScheme.secondary),
+              child: Row(
+                spacing: 15,
+                children: [
+                  CircleAvatar(
+                    radius: 28,
+                  ),
+                  Column(
+                    spacing: 4,
+                    children: [
+                      Text(
+                        'Bem-vindo, usuário',
+                        style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        spacing: 6,
+                        children: [
+                          Icon(
+                            Icons.email,
+                            color: theme.colorScheme.primary,
+                            size: 15,
+                          ),
+                          Text(
+                            'emaillegal@email.com',
+                            style: theme.textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Text('Profile'),
     );
   }
 }
