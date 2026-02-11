0bae148d7e2e355b7977ef3284d86f42d9fbd74d
Rhayssam <rhayssam.hv@gmail.com>
2026-02-11 11:52:41 -0300

    feat: add new title and contents on AboutUsPage

--- lib/ui/about_us/about_us_page.dart
+++ lib/ui/about_us/about_us_page.dart
@@ -10,33 +10,36 @@ class AboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            CustomSliverAppBar.surface(
-              title: Text('Sobre Nós'),
-              leading: AppBarBackButton(onPressed: Get.back),
-            ),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.all(20),
-              sliver: SliverToBoxAdapter(
-                child: Column(
-                  spacing: 20,
-                  children: [
-                    AboutCard(
-                      title: 'Título',
-                      subtitle: 'Descrição Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                    ),
-                    AboutCard(
-                      title: 'Título',
-                      subtitle: 'Descrição Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                    ),
-                  ],
-                ),
+      body: CustomScrollView(
+        slivers: [
+          CustomSliverAppBar.primary(
+            leading: AppBarBackButton(onPressed: Get.back),
+          ),
+          SliverPadding(
+            padding: EdgeInsetsGeometry.all(20),
+            sliver: SliverToBoxAdapter(
+              child: Column(
+                spacing: 20,
+                children: [
+                  AboutCard(
+                    title: 'Sobre o App',
+                    contents: [
+                      'Este aplicativo foi criado para simplificar o controle de vendas, estoque e produtos de forma prática e eficiente.',
+                      'A ideia é transformar organização em estratégia e dados em decisões inteligentes.',
+                      'Cada funcionalidade foi pensada para ajudar pequenos vendedores a:',
+                      '- Controlar produtos com clareza',
+                      '- Acompanhar vendas em tempo real',
+                      '- Monitorar estoque sem surpresas',
+                      '- Entender lucro de forma simples',
+                      'Mais do que um gerenciador, este app é uma ferramenta para quem quer levar seu negócio a sério — mesmo começando pequeno.',
+                      'Nosso objetivo é evoluir constantemente, trazendo melhorias que tornem a gestão cada vez mais intuitiva, segura e eficiente.',
+                    ],
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
